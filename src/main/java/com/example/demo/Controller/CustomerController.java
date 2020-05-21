a1cdc6c1912951b83eda4878459149a57a9e13a3
Itai Gramse <ithai.g@gmail.com>
2020-05-21 13:06:44 +0200

    Add full crud functionality for customer usecase

--- src/main/java/com/example/demo/Controller/CustomerController.java
+++ src/main/java/com/example/demo/Controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -39,6 +40,26 @@ public class CustomerController {
         model.addAttribute("customers", customerList);
         return "home/customer/searchCustomer";
     }
+
+    @GetMapping("/customer/deleteCustomer/{customerId}")
+    public String deleteCustomer(@PathVariable("customerId") int customerId){
+        Boolean delete = customerService.deleteCustomer(customerId);
+        return "redirect:/";
+    }
+
+    @GetMapping("/customer/updateCustomer/{customerId}")
+    public String updateCustomer(@PathVariable("customerId") int customerId, Model model){
+        model.addAttribute("customer",customerService.findCustomerById(customerId));
+        return "home/customer/updateCustomer";
+    }
+
+    @PostMapping("/customer/updateCustomer")
+    public String updateCustomer(@ModelAttribute Customer customer){
+        System.out.println(customer);
+        customerService.updateCustomer(customer);
+        return "redirect:/";
+    }
+
 
 
 
